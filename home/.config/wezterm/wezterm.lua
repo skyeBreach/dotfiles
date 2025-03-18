bc755504d4f0d4e66a748ffb840cd1ce314a3eec
skyeBreach <skyebenson@hotmail.com>
2025-03-18 06:32:05 +0000

    feat: set wezterm theme and font

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -7,7 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Batman'
+config.color_scheme = 'purplepeter'
+
+config.font =  wezterm.font("MesloLGS NF")
 
 -- and finally, return the configuration to wezterm
 return config
